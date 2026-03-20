08f057a27679f538b983fa63ff484f46f81e241d
Ayush Awasthi <98419420+enterayush@users.noreply.github.com>
2026-03-20 17:52:49 +0530

    Time: 21 ms (42.87%), Space: 46.2 MB (17.13%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -4,15 +4,15 @@ class Solution {
         int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < n - 1; j++) {
-                if (i > 0 && nums[i] == nums[i - 1])
-                    continue;
-                if (j > i+1 && nums[j] == nums[j - 1])
+                if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
                 int mid = j + 1;
                 int right = n - 1;
                 while (mid < right) {
-                    long sum = (long)nums[i] + nums[j] + nums[mid] + nums[right];
+                    long sum = (long) nums[i] + nums[j] + nums[mid] + nums[right];
                     if (sum == target) {
                         res.add(Arrays.asList(nums[i], nums[j], nums[mid], nums[right]));
                         mid++;
