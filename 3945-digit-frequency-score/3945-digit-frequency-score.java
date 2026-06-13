83e15e95973375aec50cd50e7ac312ee2c025a96
Ayush Awasthi <98419420+enterayush@users.noreply.github.com>
2026-06-14 01:05:16 +0530

    Time: 1 ms (99.29%), Space: 42.6 MB (61.38%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,15 +1,9 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        int[] arr = new int[10];
-        int dig = 0;
-        while(n != 0){
-            dig = n%10;
-            arr[dig]++;
-            n = n/10;
-        }
-        int sum =0;
-        for (int i = 0; i < 10; i++) {
-            sum += i * arr[i];
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
         }
         return sum;
     }
