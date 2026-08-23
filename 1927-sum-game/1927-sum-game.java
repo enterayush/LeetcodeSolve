c5bb0bd731af2d67ee41f7930b615ddf9b29590e
Ayush Awasthi <98419420+enterayush@users.noreply.github.com>
2026-08-24 03:14:41 +0530

    Time: 10 ms (15.83%), Space: 46.9 MB (65.47%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean sumGame(String num) {
         int n = num.length();
-        double res  = 0;
-        for(int i =0;i<n;i++){
-            int sign = (i< n/2) ? 1:-1;
+        double res = 0;
+        for (int i = 0; i < n; i++) {
+
             double value;
-            if(num.charAt(i) == '?') value = 4.5;
-            else value = num.charAt(i)-'0';
-            res += sign * value;
+            if (num.charAt(i) == '?')
+                value = 4.5;
+            else
+                value = num.charAt(i) - '0';
+            if (i < n / 2) {
+                res += value;
+            } else {
+                res -= value;
+            }
+            
         }
         return res != 0;
-
     }
 }
